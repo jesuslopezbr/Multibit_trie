3f41fbcc8928565068cf4f604f875582359708a6
Jesús López <jesuslopez@MacBook-Pro-de-Jesus.local>
2018-03-16 22:56:28 +0100

    fin_woc

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int error;
 short *f_table;
 short *s_table;
-unsigned short extended_IPs;
+unsigned short ip_ext;
 long ip_index;
-uint32_t *prefix;
+uint32_t *ip_addr;
 int *prefixLength;
 int *outInterface;
 struct timespec initialTime, finalTime;
@@ -16,7 +16,7 @@ double *totalPacketProcessingTime;
 void usage(int argc)
 {
   if(argc != 3){
-    fprintf(stderr, "\nUsage: <./my_routing_lookup> <FIB> <InputPacketFile>\n\n" );
+    fprintf(stderr, "\nUsage: <./my_routing_lookup> <FIB> <InputPacketFile>\n\n");
     exit(-1);
   }
 }
@@ -24,14 +24,14 @@ void usage(int argc)
 //FILL FIRST TABLE & SECOND TABLE
 void fill_FIB()
 {
-  prefix = calloc(1,sizeof(int));
+  ip_addr = calloc(1,sizeof(int));
 	prefixLength = calloc(1,sizeof(int));
 	outInterface = calloc(1,sizeof(int));
   long int hosts = 0;
-  extended_IPs = 0;
+  ip_ext = 0;
   ip_index = 0;
 
-  error = readFIBLine(prefix, prefixLength, outInterface);
+  error = readFIBLine(ip_addr, prefixLength, outInterface);
 
   while(error == OK)
   {
@@ -42,51 +42,51 @@ void fill_FIB()
       //Fill the interface field of the first table for each IP
       for(ip_index = 0; ip_index < hosts; ip_index++)
 			{
-				f_table[(*prefix>>8) + ip_index] = *outInterface;
+				f_table[(*ip_addr>>8) + ip_index] = *outInterface;
 			}
     }
     else
     {
       hosts = pow(2, 32 - *prefixLength);
 
-      if(f_table[*prefix>>8]>>15 == 0)
+      if(f_table[*ip_addr>>8]>>15 == 0)
       {
-        s_table = realloc(s_table, 256*(extended_IPs + 1)*2);
+        s_table = realloc(s_table, 256*(ip_ext + 1)*2);
 
         for(ip_index = 0; ip_index <= 255; ip_index++)
         {
-          s_table[extended_IPs*256 + ip_index] = f_table[*prefix>>8];
+          s_table[ip_ext*256 + ip_index] = f_table[*ip_addr>>8];
         }
 
-        f_table[*prefix>>8] = extended_IPs | 0x8000;
+        f_table[*ip_addr>>8] = ip_ext | 0x8000;
 
-        for(ip_index = (*prefix & 0xFF); ip_index < hosts + (*prefix & 0xFF); ip_index++)
+        for(ip_index = (*ip_addr & 0xFF); ip_index < hosts + (*ip_addr & 0xFF); ip_index++)
 				{
-					s_table[extended_IPs*256 + ip_index] = *outInterface;
+					s_table[ip_ext*256 + ip_index] = *outInterface;
 				}
-				extended_IPs++;
+				ip_ext++;
       }
       else
       {
-        for(ip_index = (*prefix & 0xFF); ip_index < hosts + (*prefix & 0xFF); ip_index++)
+        for(ip_index = (*ip_addr & 0xFF); ip_index < hosts + (*ip_addr & 0xFF); ip_index++)
 				{
-					s_table[(f_table[*prefix>>8] & 0x7FFF)*256 + ip_index] = *outInterface;
+					s_table[(f_table[*ip_addr>>8] & 0x7FFF)*256 + ip_index] = *outInterface;
 				}
       }
     }
     //READ ANOTHER LINE
-    error = readFIBLine(prefix, prefixLength, outInterface);
+    error = readFIBLine(ip_addr, prefixLength, outInterface);
   }
   //FREE RESOURCES
-  free(prefix);
+  free(ip_addr);
   free(prefixLength);
   free(outInterface);
 }
 
 //LOOK FOR AN INTERFACE AND A TABLE FOR AN SPECIFIC IP ADDRESS
-void lookup(uint32_t *IP_lookup, short int *numberOfTableAccesses, unsigned short *out_Interface)
+void lookup(uint32_t *ip_lookup, short int *numberOfTableAccesses, unsigned short *out_Interface)
 {
-  *out_Interface = f_table[*IP_lookup>>8];
+  *out_Interface = f_table[*ip_lookup>>8];
   if(*out_Interface>>15 == 0)
 	{
 		*numberOfTableAccesses = 1;
@@ -95,7 +95,7 @@ void lookup(uint32_t *IP_lookup, short int *numberOfTableAccesses, unsigned shor
 	else
 	{
 		*numberOfTableAccesses = 2;
-		*out_Interface = s_table[(*out_Interface & 0x7FFF)*256 + (*IP_lookup & 0x000000FF)];
+		*out_Interface = s_table[(*out_Interface & 0x7FFF)*256 + (*ip_lookup & 0x000000FF)];
 		// 0x7fff = 0b0111111111111111 to adquire just the address to the 2nd table
 		return;
 	}
@@ -105,31 +105,31 @@ void lookup(uint32_t *IP_lookup, short int *numberOfTableAccesses, unsigned shor
 //LOOK FOR THE BEST INTERFACE FOR EACH IP ADDRESS (ROUTES)
 void routing()
 {
-  uint32_t *IP_lookup = calloc(1,sizeof(uint32_t));
+  uint32_t *ip_lookup = calloc(1,sizeof(uint32_t));
 	unsigned short *out_Interface = calloc(1,sizeof(unsigned short));
 	double *searchingTime = calloc(1,sizeof(double));
 	short int *numberOfTableAccesses = calloc(1,sizeof(short int));
 
-	error = readInputPacketFileLine(IP_lookup);
+	error = readInputPacketFileLine(ip_lookup);
 
   while(error == OK)
   {
     //START TIME (INITIAL TIME)
     clock_gettime(CLOCK_REALTIME, &initialTime);
-    lookup(IP_lookup, numberOfTableAccesses, out_Interface);
+    lookup(ip_lookup, numberOfTableAccesses, out_Interface);
     //END TIME (FINAL TIME)
     clock_gettime(CLOCK_REALTIME, &finalTime);
-    printOutputLine(*IP_lookup, *out_Interface, &initialTime, &finalTime,
+    printOutputLine(*ip_lookup, *out_Interface, &initialTime, &finalTime,
                     searchingTime, *numberOfTableAccesses);
-    *processedPackets = *processedPackets + 1;
+    *processedPackets += 1;
     *totalTableAccesses  += *numberOfTableAccesses;
     *totalPacketProcessingTime  += *searchingTime;
 
     //READ ANOTHER LINE
-    error = readInputPacketFileLine(IP_lookup);
+    error = readInputPacketFileLine(ip_lookup);
   }
   //FREE RESOURCES
-  free(IP_lookup);
+  free(ip_lookup);
   free(out_Interface);
   free(searchingTime);
   free(numberOfTableAccesses);
@@ -143,9 +143,9 @@ int main(int argc, char *argv[])
 
   //RESERVE DINAMIC MEMORY
   f_table = calloc(F_TABLE_ENTRIES, sizeof(short));
-  processedPackets  = calloc(1,sizeof(int));
-	totalTableAccesses  = calloc(1,sizeof(double));
-	totalPacketProcessingTime  = calloc(1,sizeof(double));
+  processedPackets  = calloc(1, sizeof(int));
+	totalTableAccesses  = calloc(1, sizeof(double));
+	totalPacketProcessingTime  = calloc(1, sizeof(double));
   error = 0;
 
   //INITIALIZE FILES
